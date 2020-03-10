012f1f26e963fe5f8e04fe6fe872585a3f4e73ec
ColineM18 <60229117+ColineM18@users.noreply.github.com>
2020-03-10 19:00:44 +0100

    cocoline base

---
+++ ConsoleApp1/ConsoleApp1/Statics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Statics // contiendras les fonctions que tout le monde veut
+{
+
+    public static void Test() //static = accessible par tout le monde
+    {
+        string key;
+
+        //Console.WriteLine("test"); // ceci est un debug
+        ///WriteLine("texte"); //ceci est un message dans la console
+
+        //key = Console.ReadLine(); // entrer valeur
+        ///Console.ReadKey() //variante, a eviter pour l'instant
+
+        Console.WriteLine("Entrer valeur");
+        key = Console.ReadLine();
+        Console.WriteLine("Vous avez tapé " + key);
+        Console.WriteLine("Tapez exit");
+        Console.ReadLine();
+        // s'arrête automatiquement une fois le programme fini
+    }
+}
